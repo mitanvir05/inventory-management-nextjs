b183cdbb57cda0889f7da1e774556cfde6cbb886
Md. Muhaimin Islam Tanvir <tanvir.cse05@gmail.com>
2026-01-28 13:19:00 +0600

    Update page.tsx

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -4,11 +4,19 @@ import { getCurrentUser } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
 import React from "react";
 
-export default async function InventoryPage() {
+export default async function InventoryPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ q?: string }>;
+}) {
   const user = await getCurrentUser();
   const userId = user.id;
+
+  const params = await searchParams;
+  const q = (params.q ?? "").trim();
+
   const totalProducts = await prisma.product.findMany({
-    where: { userId },
+    where: { userId, name: { contains: q, mode: "insensitive" } },
   });
 
   return (
@@ -27,6 +35,19 @@ export default async function InventoryPage() {
         </div>
 
         <div className="space-y-6">
+          {/* search */}
+          <div className="bg-white rounded-lg border border-gray-200 p-4">
+            <form className="flex gap-2" action="/inventory" method="GET">
+              <input
+                name="q"
+                placeholder="Search Products....."
+                className="flex-1 px-4 py-2 border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-200"
+              />
+              <button className="px-6 py-2 bg-purple-600 text-white rounded-lg hover:bg-purple-700">
+                Search
+              </button>
+            </form>
+          </div>
           {/* Inventory items will go here */}
           <div className="bg-white rounded-lg border border-gray-200 overflow-hidden">
             <table className="w-full">
