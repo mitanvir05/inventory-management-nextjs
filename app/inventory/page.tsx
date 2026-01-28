96d1dfb4a0a0723e28ed2e5dd5328b742b1480c5
Md. Muhaimin Islam Tanvir <tanvir.cse05@gmail.com>
2026-01-28 20:25:12 +0600

    Update page.tsx

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -4,7 +4,7 @@ import { prisma } from "@/lib/prisma";
 import { Search, AlertCircle, CheckCircle2 } from "lucide-react";
 import Link from "next/link";
 import DeleteProduct from "@/components/delete-product";
-const ITEMS_PER_PAGE = 10;
+const ITEMS_PER_PAGE = 7;
 
 export default async function InventoryPage({
   searchParams,
@@ -61,7 +61,7 @@ export default async function InventoryPage({
                   name="q"
                   defaultValue={q}
                   placeholder="Search by product name..."
-                  className="w-full pl-10 pr-4 py-2.5 bg-gray-50 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500/20 focus:border-purple-500 transition-all"
+                  className="w-full pl-10 pr-4 py-2.5 bg-gray-50 border border-gray-100 rounded-lg focus:outline-none  transition-all"
                 />
               </div>
               <button className="px-6 py-2.5 bg-gray-900 text-white rounded-lg hover:bg-gray-800 transition-colors font-medium">
